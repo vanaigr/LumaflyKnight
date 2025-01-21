02dd9cb9407d10e075b1fdad1bd58b6fb1308e97
vanaigr <vanaigranov@gmail.com>
2025-01-21 14:56:24 -0600

    v1

--- ModClass.cs
+++ ModClass.cs
@@ -5,6 +5,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UObject = UnityEngine.Object;
 
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.ComponentModel.Design.Serialization;
+using HutongGames.PlayMaker.Actions;
+using System.Linq;
+using System.Reflection;
+using Modding.Converters;
+using System.IO;
+
 namespace LumaflyKnight
 {
     public class LumaflyKnight : Mod
@@ -24,13 +33,273 @@ namespace LumaflyKnight
         //    Instance = this;
         //}
 
+        void reportAll(GameObject it, string indent)
+        {
+            Log(indent + "name: " + it.name);
+            Log(indent + " components:");
+            var cs = it.GetComponents<Component>();
+            for(int i = 0; i < cs.Length; i++) {
+                Log(indent + "  " + cs[i].GetType().Name);
+            }
+            Log(indent + " children:");
+            for(int i = 0; i < it.transform.childCount; i++) {
+                reportAll(it.transform.GetChild(i).gameObject, indent + "  ");
+            }
+        }
+
+        class ContainLumafly {
+            public List<GameObject> lamps;
+            public List<GameObject> enemies;
+            //public List<GameObject> unbreakableLamps;
+        }
+
+        void addAll(GameObject it, ContainLumafly cl) {
+            var s = (string name) => it.name.StartsWith(name);
+
+            if(s("lamp_bug_escape")) cl.lamps.Add(it);
+            else if(s("Zombie Miner")) cl.enemies.Add(it);
+            else if(s("Zombie Myla")) cl.enemies.Add(it);
+            //else if(s("lamp_01")) cl.unbreakableLamps.Add(it);
+            //else if(s("lamp_02")) cl.unbreakableLamps.Add(it);
+            //else if(s("lamp_01_glows")) cl.unbreakableLamps.Add(it);
+            //else if(s("lamp_02_glows")) cl.unbreakableLamps.Add(it);
+
+            for(var i = 0; i < it.transform.childCount; i++) {
+                addAll(it.transform.GetChild(i).gameObject, cl);
+            }
+        }
+
+        static FieldInfo breakableRemnantParts;
+        private object content;
+
+        GameObject[] getRemnantParts(Breakable it) {
+            if(breakableRemnantParts == null) {
+                breakableRemnantParts = typeof(Breakable).GetField("debrisParts", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+            }
+            return ((List<GameObject>)breakableRemnantParts.GetValue(it)).ToArray();
+        }
+
+        GameObject isBreakable(GameObject it, HashSet<GameObject> possibleRemnants) {
+            var p = it.transform.parent;
+            if(p == null) { 
+                return null;
+            }
+
+            possibleRemnants.Add(it);
+
+            var b = p.GetComponent<Breakable>();
+            if(b) {
+                var list = getRemnantParts(b);
+                var i = 0;
+                for(; i < list.Length; i++) {
+                    if (possibleRemnants.Contains(list[i])) break;
+                }
+                
+                if(i != list.Length) {
+                    return p.gameObject.activeInHierarchy ? p.gameObject : null;
+                }
+
+                return null;
+            }
+            else {
+                return isBreakable(p.gameObject, possibleRemnants);
+            }
+        }
+
+        /*IEnumerator updateCount() {                
+            var s0 = UnityEngine.SceneManagement.SceneManager.GetActiveScene();
+            var s0name = s0.name;
+            yield return null;
+            var s = UnityEngine.SceneManagement.SceneManager.GetActiveScene();
+            if(s != s0) {
+                Log("Should not happen 1: " + s0name + " " + s.name);
+                yield break;
+            }
+            Log("Room name: " + s.name + " " + s.buildIndex);
+
+            try { 
+                var rs = s.GetRootGameObjects();
+                var lumas = new ContainLumafly { 
+                    enemies = new List<GameObject>(), 
+                    lamps = new List<GameObject>(),
+                    unbreakableLamps = new List<GameObject>(),
+                };
+                for(var i = 0; i < rs.Length; i++) {
+                    addAll(rs[i], lumas);
+                }
+                Log("Count is " + lumas.lamps.Count + " " + lumas.enemies.Count);
+
+                var hitCount = 0;
+                var possibleCount = 0;
+
+                for(var i = 0; i < lumas.lamps.Count; i++) {
+                    var l = lumas.lamps[i];
+                    try {
+                        Log("checking " + i);
+                        if(isBreakable(l, new HashSet<GameObject>())) { 
+                            var u = l.AddComponent<UpdateWhenActive>();
+                            u.onEnable += (_, _) => {
+                                hitCount++;
+                                Ui.getUi()?.UpdateStats(hitCount, possibleCount);
+                            };
+                            possibleCount++;
+                        }
+                        else {
+                            lumas.unbreakableLamps.Add(l);
+                        }
+                    }
+                    catch(Exception e) {
+                        LogError("Died on lamp " + s.name + " " + l.name + ": " + e);
+                    }
+                }
+
+                for(var i = 0; i < lumas.enemies.Count; i++) {
+                    var l = lumas.enemies[i];
+                    try {
+                        l.GetComponent<HealthManager>().OnDeath += () => {
+                            hitCount++;
+                                Ui.getUi()?.UpdateStats(hitCount, possibleCount);
+                        };
+                            possibleCount++;
+                    }
+                    catch(Exception e) {
+                        LogError("Died on enemy " + s.name + " " + l.name + ": " + e);
+                    }
+                }
+
+                for(var i = 0; i < lumas.unbreakableLamps.Count; i++) {
+                    var l = lumas.unbreakableLamps[i];
+                    try {
+                        l.transform.localScale = l.transform.localScale * 3;
+                    }
+                    catch(Exception e) {
+                        LogError("Died on unbreakable " + s.name + " " + l.name + ": " + e);
+                    }
+                }
+
+                Ui.getUi()?.UpdateStats(hitCount, possibleCount);
+            }
+            catch(Exception e) {
+                LogError("Counting died: " + e);
+            }
+
+            yield break;
+        }*/
+
+        public static string path(GameObject obj) {
+            string path = "/" + obj.name;
+            while (obj.transform.parent != null) {
+                obj = obj.transform.parent.gameObject;
+                path = "/" + obj.name + path;
+            }
+            return path;
+        }
+
+        SceneObjects saveSceneObjects(Scene s) {
+            var res = new SceneObjects();
+
+            var lumas = new ContainLumafly { 
+                enemies = new List<GameObject>(), 
+                lamps = new List<GameObject>(),
+            };
+            
+            var rs = s.GetRootGameObjects();
+            for(var i = 0; i < rs.Length; i++) {
+                addAll(rs[i], lumas);
+            }
+            Log("  Count is " + lumas.lamps.Count + " " + lumas.enemies.Count);
+
+            for(var i = 0; i < lumas.lamps.Count; i++) {
+                var l = lumas.lamps[i];
+                var root = isBreakable(l, new HashSet<GameObject>());
+                if(root != null) { 
+                    res.lamps.Add(path(l), new LampData { deletePath = path(root) });
+                }
+            }
+
+            for(var i = 0; i < lumas.enemies.Count; i++) {
+                var l = lumas.enemies[i];
+                res.enemies.Add(path(l), new EnemyData());
+            }
+
+            return res;
+        }
+
+        private struct LampData {
+            public string deletePath;
+        }
+
+        private struct EnemyData {
+        }
+
+
+        private class SceneObjects {
+            public Dictionary<string, LampData> lamps = new Dictionary<string, LampData>();
+            public Dictionary<string, EnemyData> enemies = new Dictionary<string, EnemyData>();
+        }
+
+        IEnumerator doStuff() {
+            var sceneCount = UnityEngine.SceneManagement.SceneManager.sceneCountInBuildSettings;
+            var scenes = new Scene[sceneCount];
+            Log("there's " + sceneCount + " scenes.");
+            var result = new Dictionary<string, SceneObjects>();
+            for(int i = 0; i < sceneCount; i++) {
+                UnityEngine.SceneManagement.SceneManager.LoadScene(i);
+                yield return null; // we DO need to await for individual scenes and not load all + wait + process all
+                var s = UnityEngine.SceneManagement.SceneManager.GetSceneByBuildIndex(i);
+                Log("  scene " + i + " is named " + s.name);
+                var sr = saveSceneObjects(s);
+                if(sr.lamps.Count > 0 || sr.enemies.Count > 0) {
+                    result.Add(s.name, sr);
+                }
+                UnityEngine.SceneManagement.SceneManager.UnloadScene(i);
+                //var roots = s.GetRootGameObjects();
+                //for (int j = 0; j < roots.Length; j++) {
+                //    reportAll(roots[j], "");
+                //}
+            }
+
+            var resS = Newtonsoft.Json.JsonConvert.SerializeObject(result);
+            File.WriteAllText("C:\\Users\\Artem\\Downloads\\HKMODS\\LumaflyKnight\\all.json", resS);
+
+            /*var l = new List<string>();
+            l.Add("Crossroads_27");
+            for(int i = 0; i < l.Count; i++) {
+                UnityEngine.SceneManagement.SceneManager.LoadScene(l[i]);
+                yield return null;
+                var s = UnityEngine.SceneManagement.SceneManager.GetSceneByName(l[i]);
+                Log("  scene " + i + " is named " + s.name);
+                var roots = s.GetRootGameObjects();
+                for (int j = 0; j < roots.Length; j++) {
+                    reportAll(roots[j], "");
+                }
+            }*/
+            
+            UnityEngine.Application.Quit(0);
+
+            RegisterUi.add();
+
+            //UnityEngine.SceneManagement.SceneManager.activeSceneChanged += (_, _) => GameManager.instance.StartCoroutine(updateCount());
+
+            yield break;
+        }
+
         public override void Initialize(Dictionary<string, Dictionary<string, GameObject>> preloadedObjects)
         {
             Log("Initializing");
 
-            Instance = this;
+            LumaflyKnight.Instance = this;
+
+            GameManager.instance.StartCoroutine(doStuff());
 
             Log("Initialized");
+        }
+    }
+
+    class UpdateWhenActive : MonoBehaviour {
+        public event EventHandler onEnable;
+        public void OnEnable() {
+            onEnable?.Invoke(this, EventArgs.Empty);
         }
     }
 }
---
+++ Ui.cs
@@ -0,0 +1,234 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LumaflyKnight
+{
+    class Ui : MonoBehaviour {
+        private class RowLayoutObject {
+            public float MinWidth = 0;
+            public float WidthStepSize;
+            public float PaddingRight = 0;
+
+            public GameObject GameObject_ = null;
+
+            public float GetRealWidth() {
+                Renderer renderer = GameObject_?.GetComponent<Renderer>();
+                if (renderer == null) {
+                    throw new InvalidOperationException(
+                        "GameObject_ must be non-null and have a renderer.");
+                }
+
+                Transform parentTransform = GameObject_.transform.parent;
+                if (parentTransform == null) {
+                    return renderer.bounds.size.x;
+                } else {
+                    Vector3 localSize =
+                        parentTransform.InverseTransformVector(
+                            renderer.bounds.size);
+                    return localSize.x;
+                }
+            }
+
+            public float GetComputedWidth() {
+                float realWidth = GetRealWidth();
+                float paddedRealWidth = realWidth + PaddingRight;
+                float unroundedComputedWidth = Mathf.Max(
+                    paddedRealWidth, MinWidth);
+                if (unroundedComputedWidth <= MinWidth) {
+                    return MinWidth;
+                } else if (WidthStepSize <= 0) {
+                    return unroundedComputedWidth;
+                } else {
+                    return MinWidth + WidthStepSize * Mathf.Ceil(
+                        (unroundedComputedWidth - MinWidth) / WidthStepSize);
+                }
+            }
+        }
+
+        // The first object is the "anchor", it will not be moved but its
+        // computed width will be used.
+        private List<RowLayoutObject> _layout = new List<RowLayoutObject>();
+
+        // The normal size of the geo count is rather large, such that adding
+        // a bunch of grass stats next to it is overwhelmingly large. So we
+        // scale it down by this factor.
+        public float Scale = 0.6f;
+
+        private GameObject _roomCount = null;
+        private GameObject _globalCount = null;
+
+        public void Start() {
+            try {
+                _Start();
+            } catch (System.Exception e) {
+                LumaflyKnight.Instance.Log("Error in Ui.Start() " + e);
+            }
+        }
+
+        private void _Start() {
+            _layout.Add(new RowLayoutObject {
+                MinWidth = 1.4f, // A bit wider than 3 digits
+                WidthStepSize = 0.5f, // Roughly 1 digit
+                PaddingRight = 0.7f,
+                GameObject_ = GetGeoTextObject(),
+            });
+            _roomCount = CreateTextObject("Room Grass Count");
+            _layout.Add(new RowLayoutObject {
+                MinWidth = 1.4f,
+                WidthStepSize = 0.5f,
+                PaddingRight = 0.7f,
+                GameObject_ = _roomCount,
+            });
+            _globalCount = CreateTextObject("Global Grass Count");
+            _layout.Add(new RowLayoutObject {
+                MinWidth = 0,
+                WidthStepSize = 0,
+                PaddingRight = 0,
+                GameObject_ = _globalCount,
+            });
+        }
+
+        private GameObject CreateTextObject(string name) {
+            GameObject result = new GameObject(
+                name, typeof(TextMesh), typeof(MeshRenderer));
+            result.layer = gameObject.layer;
+            UnityEngine.Object.DontDestroyOnLoad(result);
+
+            GameObject geoTextObject = GetGeoTextObject();
+
+            TextMesh geoTextMesh = geoTextObject.GetComponent<TextMesh>();
+            TextMesh textMesh = result.GetComponent<TextMesh>();
+            textMesh.alignment = TextAlignment.Left;
+            textMesh.anchor = TextAnchor.MiddleLeft;
+            textMesh.font = geoTextMesh.font;
+            textMesh.fontSize = geoTextMesh.fontSize;
+            textMesh.text = geoTextMesh.text;
+
+            MeshRenderer meshRenderer = result.GetComponent<MeshRenderer>();
+            meshRenderer.material = textMesh.font.material;
+            meshRenderer.enabled = false;
+
+            result.transform.parent = gameObject.transform;
+            result.transform.localScale = geoTextObject.transform.localScale;
+            result.transform.localPosition = geoTextObject.transform.localPosition;
+
+            return result;
+        }
+
+        public void Destroy() {
+            try {
+                UnityEngine.Object.Destroy(_roomCount);
+            } catch (System.Exception e) {
+                LumaflyKnight.Instance.Log("Error in Ui.Destroy() " + e);
+            }
+        }
+
+        void Update() {
+            try {
+                _Update();
+            } catch (System.Exception e) {
+                LumaflyKnight.Instance.Log("Error in Ui.Update()"  + e);
+            }
+        }
+
+        void _Update() {
+            MaybeResize();
+            ReflowLayout();
+        }
+
+        static Vector3 WithX(Vector3 v, float newX) {
+            return new Vector3(newX, v.y, v.z);
+        }
+
+        void ReflowLayout() {
+            if (_layout.Count <= 0) {
+                return;
+            }
+
+            // The first component (the anchor) isn't created by us, and its
+            // position is the center of itself (I think), so we use the
+            // Renderer's bounds to get its leftmost edge.
+            Transform anchorParentTransform =
+                _layout[0].GameObject_.transform.parent;
+            float anchorLeft = anchorParentTransform.InverseTransformPoint(
+                _layout[0].GameObject_.GetComponent<Renderer>().bounds.min).x;
+
+            float currentX = anchorLeft + _layout[0].GetComputedWidth();
+            for (int i = 1; i < _layout.Count; ++i) {
+                Transform transform = _layout[i].GameObject_.transform;
+                transform.localPosition = WithX(transform.localPosition,
+                                                currentX);
+                currentX += _layout[i].GetComputedWidth();
+                _layout[i].GameObject_.GetComponent<Renderer>().enabled = true;
+            }
+        }
+
+        // Makes the counter smaller
+        void MaybeResize() {
+            if (gameObject.transform.localScale.x == Scale && 
+                    gameObject.transform.localScale.y == Scale &&
+                    gameObject.transform.localScale.z == Scale) {
+                // Nothing to do!
+                return;
+            }
+
+            // Fix the top left of the Geo Sprite in place as we shrink it
+            GameObject child = GetSpriteObject();
+            Vector2 startPosition = TopLeftOfBounds(
+                child.GetComponent<Renderer>().bounds);
+            gameObject.transform.localScale = Vector3.one * Scale;
+            gameObject.transform.position -= (Vector3)(
+                TopLeftOfBounds(child.GetComponent<Renderer>().bounds) -
+                startPosition);
+        }
+
+        Vector2 TopLeftOfBounds(Bounds bounds) {
+            return new Vector2(bounds.min.x, bounds.max.y);
+        }
+
+        GameObject GetGeoTextObject() {
+            GameObject result = gameObject.transform.Find("Geo Text").gameObject;
+            if (result == null) {
+                throw new InvalidOperationException("Cannot find Geo Text.");
+            }
+
+            return result;
+        }
+
+        GameObject GetSpriteObject() {
+            GameObject result = gameObject.transform.Find("Geo Sprite").gameObject;
+            if (result == null) {
+                throw new InvalidOperationException("Cannot find Geo Sprite.");
+            }
+
+            return result;
+        }
+
+        public void UpdateStats(int hits, int count) {
+            _roomCount.GetComponent<TextMesh>().text = hits + "/" + count;
+            //_globalCount.GetComponent<TextMesh>().text = "aboba2";
+        }
+        public static Ui getUi() {
+            GameObject geoCounter = GameManager.instance?.hero_ctrl?.geoCounter?.gameObject;
+             if (geoCounter != null) {
+                var c = geoCounter.GetComponent<Ui>();
+                if(c == null) c = geoCounter.AddComponent<Ui>();
+                return c;
+             }
+             return null;
+        }
+    }
+
+    class RegisterUi : MonoBehaviour {
+   
+
+        public void Update() {
+            Ui.getUi();
+        }
+
+        public static void add() { 
+            GameManager.instance.gameObject.AddComponent<RegisterUi>();
+        }
+    }
+}
